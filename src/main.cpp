794a11e952ddfd357c47c15d2bff969cad022353
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-03-26 12:12:16 +0100

    blink functions

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,57 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN         D6          // Configurable, see typical pin layout above
-#define SS_PIN          D4       // Configurable, see typical pin layout above
-#define GLED            D1
-#define RLED            D2
+#define RST_PIN D6 // Configurable, see typical pin layout above
+#define SS_PIN D4  // Configurable, see typical pin layout above
+#define GLED D1
+#define RLED D2
 
-MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 
 bool rfid_tag_present_prev = false;
 bool rfid_tag_present = false;
 int _rfid_error_counter = 0;
 bool _tag_found = false;
 
-void setup() {
-	Serial.begin(115200);
-	while (!Serial);
-	SPI.begin();
-	mfrc522.PCD_Init();		// Init MFRC522
+void Blink(int count, int led)
+{
+  int i = 0;
+  while (i < count)
+  {
+    digitalWrite(led, HIGH);
+    delay(500);
+    digitalWrite(led, LOW);
+    delay(500);
+    i++;
+  }
 }
 
-void loop() {
+void blinkgreen(int count)
+{
+  Blink(count, GLED);
+}
+
+void blinkred(int count)
+{
+  Blink(count, RLED);
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+    ;
+  SPI.begin();
+  mfrc522.PCD_Init(); // Init MFRC522
+}
+
+void loop()
+{
   rfid_tag_present_prev = rfid_tag_present;
 
   _rfid_error_counter += 1;
-  if(_rfid_error_counter > 2){
+  if (_rfid_error_counter > 2)
+  {
     _tag_found = false;
   }
 
@@ -41,24 +68,28 @@ void loop() {
 
   MFRC522::StatusCode result = mfrc522.PICC_RequestA(bufferATQA, &bufferSize);
 
-  if(result == mfrc522.STATUS_OK){
-    if ( ! mfrc522.PICC_ReadCardSerial()) { //Since a PICC placed get Serial and continue   
+  if (result == mfrc522.STATUS_OK)
+  {
+    if (!mfrc522.PICC_ReadCardSerial())
+    { // Since a PICC placed get Serial and continue
       return;
     }
     _rfid_error_counter = 0;
-    _tag_found = true;        
+    _tag_found = true;
   }
-  
+
   rfid_tag_present = _tag_found;
-  
+
   // rising edge
-  if (rfid_tag_present && !rfid_tag_present_prev){
+  if (rfid_tag_present && !rfid_tag_present_prev)
+  {
     Serial.println("Tag found");
     mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
   }
-  
+
   // falling edge
-  if (!rfid_tag_present && rfid_tag_present_prev){
+  if (!rfid_tag_present && rfid_tag_present_prev)
+  {
     Serial.println("Tag gone");
   }
   delay(100);
