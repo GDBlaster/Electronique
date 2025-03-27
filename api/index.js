8bc42c2d8093513a30ce3b0f497d6ef4f0ba3820
GDBlaster <65135527+GDBlaster@users.noreply.github.com>
2025-03-27 11:29:45 +0100

    /check

--- api/index.js
+++ api/index.js
@@ -1,18 +1,34 @@
 import express from "express";
+import db from "./db.json"
 
 const app = express();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log("server listening on port", PORT);
 });
 
-app.get("/ping" , (request , response) => {
+app.get("/ping", (request, response) => {
     var status = {
-        "Ping" : "Pong"
+        "Ping": "Pong"
     }
 
     response.send(status);
 });
 
+app.get("/check", (request, response) => {
+    if (request.body.token in db.tokens) {
+        if (db.badges?.[request.body.id] != undefined) {
+            var out = {
+                "permission" : db.badges[request.body.id],
+            }
+            response.send(out)
+        } else {
+            response.status(404).json({error : "Not Found"});
+        };
+    } else {
+        response.status(403).json({ error : "Forbiden"});
+    };
+
+});
