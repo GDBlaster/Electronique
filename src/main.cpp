fe6739f8bbe6718a638924bde50820de86f8dcfa
eliott brehin <ebrehin@guardiaschool.fr>
2025-04-02 15:31:48 +0200

    ça marche

--- src/main.cpp
+++ src/main.cpp
@@ -71,59 +71,56 @@ void blinkred(int count)
 void mac_address(uint8_t *key) {
     uint8_t mac[6];
     WiFi.macAddress(mac);
-    // Compléter les 32 octets avec l'adresse MAC
+
+    // Compléter les 32 octets avec l'adresse MAC répétée
     for (int i = 0; i < 32; i++) {
         key[i] = mac[i % 6];
     }
 }
 
-String encrypt(const char *data)
-{
-  uint8_t key[32];
-  uint8_t iv[16];
-  char plaintext[256];
-  char encrypted[256];
-  
-  mac_address(key);
-  
-  
+String encrypt(const char *data) {
+    uint8_t key[32];
+    uint8_t iv[16] = {0};  
+    char plaintext[256];
+    char encrypted[256];
 
-  Serial.println();
-  memset(iv, 0, sizeof(iv));
-  memset(key, 0, sizeof(key));
+    mac_address(key);  
+    
 
-  memset(plaintext, 0, sizeof(plaintext));
-  strcpy(plaintext, data);
+    memset(plaintext, 0, sizeof(plaintext));
+    strcpy(plaintext, data);
 
-  esp_aes_context ctx;
-  esp_aes_init(&ctx);
-  esp_aes_setkey(&ctx, key, 256);
+    esp_aes_context ctx;
+    esp_aes_init(&ctx);
+    esp_aes_setkey(&ctx, key, 256);
 
-  esp_aes_crypt_cbc(&ctx, ESP_AES_ENCRYPT, sizeof(plaintext), iv, (uint8_t *)plaintext, (uint8_t *)encrypted);
-  esp_aes_free(&ctx);
-  return String(encrypted);
+    esp_aes_crypt_cbc(&ctx, ESP_AES_ENCRYPT, sizeof(plaintext), iv, (uint8_t *)plaintext, (uint8_t *)encrypted);
+    esp_aes_free(&ctx);
+
+    //Serial.printf("Encrypted text: %s\n", encrypted);
+    return String(encrypted);
 }
 
-String decrypt(String input_text, int size)
-{
-  uint8_t key[32];
-  uint8_t iv[16];
-  uint8_t encrepted_text[size];
-  char result[size + 1];
-  
-  mac_address(key);
+String decrypt(String input_text, int size) {
+    uint8_t key[32];
+    uint8_t iv[16] = {0};  
+    uint8_t encrypted_text[size];
+    char result[size + 1];
 
-  memcpy(encrepted_text, input_text.c_str(), input_text.length());
-  memset(iv, 0, sizeof(iv));
-  memset(key, 0, sizeof(key));
+    mac_address(key);  
+    
 
-  esp_aes_context ctx;
-  esp_aes_init(&ctx);
-  esp_aes_setkey(&ctx, key, 256);
-  esp_aes_crypt_cbc(&ctx, ESP_AES_DECRYPT, size, iv, (uint8_t *)encrepted_text, (uint8_t *)result);
-  esp_aes_free(&ctx);
-  printf("Decrypted text: %s\n", result);
-  return String(result);
+    memcpy(encrypted_text, input_text.c_str(), input_text.length());
+
+    esp_aes_context ctx;
+    esp_aes_init(&ctx);
+    esp_aes_setkey(&ctx, key, 256);
+    esp_aes_crypt_cbc(&ctx, ESP_AES_DECRYPT, size, iv, encrypted_text, (uint8_t *)result);
+    esp_aes_free(&ctx);
+
+    result[size] = '\0';  // Assurer la fin de chaîne pour affichage correct
+    //Serial.printf("Decrypted text: %s\n", result);
+    return String(result);
 }
 
 void initialisation()
@@ -131,7 +128,9 @@ void initialisation()
   String jwt_enc = encrypt("khgyuikjhgytujnbhgtyuijh");
   String ssid_enc = encrypt("Suu");
   String passwd_enc = encrypt("popallec");
-  String url_enc = encrypt("http://192.168.50.23:3000/check");
+  String url_enc = encrypt("http://192.168.253.23:3000/check");
+  Serial.println("url enc");
+  Serial.println(url_enc);
 
   preferences.begin("myApp", false);
   preferences.putString("token", jwt_enc);
@@ -243,7 +242,7 @@ void setup()
 {
   Serial.begin(115200);
   delay(100);
-  // initialisation();
+  //initialisation();
   lastTimeCheck = millis() - interval;
   Credentials creds = get_from_nvs_credit();
   
@@ -297,6 +296,8 @@ void checkTime()
 void loop()
 {
   checkTime();
+  
+
   if (lecturebadge == true)
   {
     rfid_tag_present_prev = rfid_tag_present;
